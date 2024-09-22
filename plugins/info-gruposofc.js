41b5d7eae2f5597d3655a4e89fad6e895fa0d144
Diomar-s <diomarlugo405@gmail.com>
2024-09-22 17:02:32 -0400

    Add files via upload

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,30 +2,24 @@ let media = './src/Grupo.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-let str = `*📍 GRUPO OFICIAL*
+let str = `*💥 𝐆𝐫𝐮𝐩𝐨𝐬 𝖔𝖋𝖎𝖈𝖎𝖆𝖑𝖊𝖘 𝐃𝐫𝐚𝐤𝐨*
 
-   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌸̶̩ܻᝒ̷̸꯭͙𝝡꯭𝝣꯭𝗚꯭𝗨꯭𝗠꯭𝗜꯭𝗡᭄𓆩֟֯፝𓆪𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
-  ┃🧸❏ ${gp1}
+   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙👑𝐃𝐫𝐚𝐤𝐨-𝐁𝐨𝐭❄︎⋰⋰𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
+  ┃👑❏ ${gp1}
 
-   *_ ͟͞〾⃝̵͡𝑬⃗ꭙȾ⃯𝑟𝘦𝑚⃖𝚎֟֯፝͜❀̵⃕ ̤🄷𝕖ᷨɳͦᵵͭ𝐚͢𝖏♡̵̭̭̎͟͞ ₂₀፝֟֯࣪࣪࣪₁₂_*
-┃🧸❏ https://chat.whatsapp.com/J9gyFJLbhVIJXaUZlpo8Xt
+   *♥︎👑⃟፝ᰯ🌹𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒄 𝒅𝒓𝒂𝒌𝒐🌹፝⃟ᰯུ👑♥︎*
+┃🪄❏ https://chat.whatsapp.com/Jjs2l4X3LdP7RHr06WsasW
    
-   *_Grupo de antojar 3.0_*
-┃🧸❏ https://chat.whatsapp.com/LJKcR8QBJgu37bVFWuhRVn
+   *♥︎👑⃟፝ᰯ🌹𝑮𝒓𝒖𝒑𝒐 𝑺𝒖𝒃𝒃𝒐𝒕𝒔🌹፝⃟ᰯུ👑♥︎*
+┃🪄❏ https://chat.whatsapp.com/Db9TD8uwhwKIKZmIxDd63e
 
-   *_Grupo Sunlight - Team _*
-┃❤️‍🔥❏ https://chat.whatsapp.com/Fy74b6fgE9SJJpHVi6CKJY
-
-   *_Canal Oficial_*
-┃❤️‍🔥❏ https://whatsapp.com/channel/0029VacDy0R6hENqnTKnG820
-
-   *_Canal Sunlight - Team_*
-┃❤️‍🔥❏ https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z
+   *♥︎👑⃟፝ᰯ🌹ʕ•ᴥ•ʔＤＲＡＫＯ Ｘ ＫＡＫＡＲＯＴＯ★᭄ꦿ᭄ꦿ🌹፝⃟ᰯུ👑♥︎*
+┃🪄❏ https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t
 *_╰━━━━━━━━━━━━━━━━⊜_*
 `
-await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝘿᪶𝙞ᷨ𝙖ᷞ𝙢ͣ𝙤᪶ͨ𝙣ᷜ𝙙ꫂৎ୭࠱࠭ ͟͞\n` + wm, media, [
-['Menu Lista 💖', '/lista']], null, [
-['⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐', `${md}`]], fkontak)}
+await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝑫𝒓𝒂𝒌𝒐-𝑩𝒐𝒕⋰⋰𝕭𝖞 𝐊𝐢𝐧𝐠ꫂৎ୭࠱࠭ \n` + wm, media, [
+['Menu Lista 👑', '/lista']], null, [
+['⏤Drako Bot', `${md}`]], fkontak)}
                       
 handler.command = ['grupos','linksk','gruposofc','gruposoficiales']
 handler.register = true
