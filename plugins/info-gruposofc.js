70d2750b9f345bd5ab45ca85fa9b971ac3a260bb
Diomar-s <diomarlugo405@gmail.com>
2024-09-22 17:04:48 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,12 +2,12 @@ let media = './src/Grupo.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-let str = `*💥 𝐆𝐫𝐮𝐩𝐨𝐬 𝖔𝖋𝖎𝖈𝖎𝖆𝖑𝖊𝖘 𝐃𝐫𝐚𝐤𝐨*
+let str = `*💥 𝐆𝐫𝐮𝐩𝐨𝐬 𝖔𝖋𝖎𝖈𝖎𝖆𝖑𝖊𝖘 𝙺𝙰𝙺𝙰𝚁𝙾𝚃𝙾*
 
    *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙👑𝐃𝐫𝐚𝐤𝐨-𝐁𝐨𝐭❄︎⋰⋰𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
   ┃👑❏ ${gp1}
 
-   *♥︎👑⃟፝ᰯ🌹𝑮𝒓𝒖𝒑𝒐 𝑶𝒇𝒄 𝒅𝒓𝒂𝒌𝒐🌹፝⃟ᰯུ👑♥︎*
+   *♥︎👑⃟፝ᰯ🌹𝙶𝚁𝚄𝙿𝙾 𝙺𝙰𝙺𝙰𝚁𝙾𝚃𝙾🌹፝⃟ᰯུ👑♥︎*
 ┃🪄❏ https://chat.whatsapp.com/Jjs2l4X3LdP7RHr06WsasW
    
    *♥︎👑⃟፝ᰯ🌹𝑮𝒓𝒖𝒑𝒐 𝑺𝒖𝒃𝒃𝒐𝒕𝒔🌹፝⃟ᰯུ👑♥︎*
@@ -17,9 +17,9 @@ let str = `*💥 𝐆𝐫𝐮𝐩𝐨𝐬 𝖔𝖋𝖎𝖈𝖎𝖆𝖑𝖊𝖘 �
 ┃🪄❏ https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t
 *_╰━━━━━━━━━━━━━━━━⊜_*
 `
-await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝑫𝒓𝒂𝒌𝒐-𝑩𝒐𝒕⋰⋰𝕭𝖞 𝐊𝐢𝐧𝐠ꫂৎ୭࠱࠭ \n` + wm, media, [
+await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝙺𝙰𝙺𝙰𝚁𝙾𝚃𝙾⋰⋰𝕭𝖞 𝐊𝐢𝐧𝐠ꫂৎ୭࠱࠭ \n` + wm, media, [
 ['Menu Lista 👑', '/lista']], null, [
-['⏤Drako Bot', `${md}`]], fkontak)}
+['⏤𝙺𝙰𝙺𝙰𝚁𝙾𝚃𝙾', `${md}`]], fkontak)}
                       
 handler.command = ['grupos','linksk','gruposofc','gruposoficiales']
 handler.register = true
