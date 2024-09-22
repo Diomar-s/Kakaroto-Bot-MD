4802adc506e79d35e9fab8767b963486702a5a65
Diomar-s <diomarlugo405@gmail.com>
2024-09-22 16:55:04 -0400

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 let name = await conn.getName(who)
 
   await sendContactArray(conn, m.chat, [
-     [`${nomorown}`, `👑 Propietario`, `ᥫᩣᎠ꯭I𝚫⃥꯭M꯭Ꭷ꯭Ꮑ꯭Ꭰ࠭⋆̟(◣_◢)凸`, dev, correo, `🇨🇺 Cuba`, `${global.yt}`, bio],
-[`${conn.user.jid.split('@')[0]}`, `Es Una Bot 🍟`, `${packname}`, `📵 No Hacer Spam`, correo, `🇨🇺 Cuba`, md, biobot]
+     [`${nomorown}`, `👑 Propietario`, `☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆`, dev, correo, `🇨🇴 Mexico`, `${global.yt}`, bio],
+[`${conn.user.jid.split('@')[0]}`, `Es Un Bot 🍟`, `${packname}`, `📵 No Hacer Spam`, correo, `🇲🇽 Mexico`, md, biobot]
 ], m)
   //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
   } 
