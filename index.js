c15d9214338e93f8fa76e380e5279cf8cb98a2d9
Diomar-s <diomarlugo405@gmail.com>
2024-09-23 11:20:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Megumin ✰');
+console.log('\n✰ Iniciando Kakaroto ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Megumin-Bot', {
+say('Kakaroto-Bot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • David-Chian`, {
+say(`Developed By • legendary`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
