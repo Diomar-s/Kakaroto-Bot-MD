de7ef0fb8e3783557d710d267b4a94885530edae
Diomar-s <diomarlugo405@gmail.com>
2024-09-23 00:25:45 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -733,15 +733,15 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> David-Chian.', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary*\n\n*_Siguenos https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t.', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
 mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.*', 
 premium: '「🍧」 *Esta función solo es para usuarios Premium.', 
-group: '「🐢」 *Esta funcion solo puede ser ejecutada en grupos.*', 
+group: '「🐢」 *Esta funcion solo puede ser ejecutada en grupos.**\n\n*_Siguenos https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
 private: '「🍭」 *Esta función solo puede ser usada en chat privado.*', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*', 
 botAdmin: '「🚩」 *Para usar esta función debo ser admin.*', 
-unreg: '「🍟」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg David.23*',
+unreg: '「🍟」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg legendary.18**\n\n*_Siguenos https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t',
 restrict: '「💫」 *Esta característica esta desactivada.*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
