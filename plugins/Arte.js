cbbb093fb350a53930c9859a194aa6620bcc197b
Diomar-s <diomarlugo405@gmail.com>
2024-09-23 23:17:48 -0400

    Add files via upload

---
+++ plugins/Arte.js
@@ -0,0 +1,3 @@
+case "Dan":
+sock.sendMessage(from, {text: `Acá agendas las reglas de Goku black`, quoted: info,txt, m, null, rcanal})
+ break
---
+++ plugins/Juegos+18.js
@@ -0,0 +1,28 @@
+//Código creado por elias1524
+
+var handler = async m => 
+
+conn.reply(m.chat, ` 
+┏━━━━━━━━━━━━━━━━━━━━┅
+┇  *「 LINK DE LOS JUEGUITOS +18」*       
+┣━━━━━━━━━━━━━━━━━━━━┅
+┃ ❖ https://www.mediafire.com/file/j4p80gd9f2mkc88/specialharemclass-release.apk/file
+┃ ❖ https://apkadmin.com/21u7gag3m6t2/pro.filer-1.0-1715607887-release.apk.html
+┃ ❖ https://www.mediafire.com/file_premium/zed50om1lc2gebj/Hillside-v0.15-Mods-Esp-Apk.rar/file?dkey=d6gj30q935n&r=
+┃ ❖ https://www.mediafire.com/file/zvtied9xb01rhue/GameOfWhores-v0.27-Esp-Apk.rar/file
+┃ ❖ https://www.mediafire.com/file/zvtied9xb01rhue/GameOfWhores-v0.27-Esp-Apk.rar/file
+┃ ❖ https://www.mediafire.com/file_premium/72n7qk5awdezrpz/IWannaFockMyMomsBestFirend-Esp-Apk.rar/file
+┃ ❖NO OLVIDEN DE SEGUIRME EN MI CANAL<3
+┃Créditos:Starrhotss
+ ${uptime}  ${paypal}
+┗━━━━━━━━━━━━━━━━━━━━
+ `, m, rcanal, )
+ 
+handler.help = ['M']
+handler.tags = ['m']
+handler.command = /^M$/i
+ 
+handler.limit = true
+handler.register = true
+ 
+export default handler 
