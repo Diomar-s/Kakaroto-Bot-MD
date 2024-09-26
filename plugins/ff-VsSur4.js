2d677d4e1fbca3bf0e33550534f9573592795028
Diomar-s <diomarlugo405@gmail.com>
2024-09-26 18:28:40 -0400

    Add files via upload

---
+++ plugins/ff-VsSur4.js
@@ -0,0 +1,24 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Sur4);
+};
+handler.command = /^(Sur4)$/i;
+handler.tags =['free'] 
+handler.help = ['Sur4'] 
+export default handler;
+global.Sur4 = ` 
+╭┈┈ *VERSUS 4x4*
+│
+│      • _Encargadx:_  
+│      • _Horario:_ (  )🇵🇪 (  )🇦🇷
+│      • _Modo:_ 
+│
+│  *〽️ESCUADRA*
+│ㅤ👤• 
+│ㅤ👤• 
+│ㅤ👤• 
+│ㅤ👤• 
+│
+│ _⚜️Donante de Sala:_
+│
+╰──────────────
+`;
