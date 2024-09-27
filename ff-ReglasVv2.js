22da4081a4f5b540e2620c5a1230dcaf18571aa3
Diomar-s <diomarlugo405@gmail.com>
2024-09-26 21:32:30 -0400

    Add files via upload

---
+++ ff-ReglasVv2.js
@@ -0,0 +1,51 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Vv2);
+};
+handler.command = /^(Vv2)$/i;
+handler.tags =['free'] 
+handler.help = ['Vv2'] 
+export default handler;
+global.Vv2 = ` 
+Reglas Vivido 
+
+primera sala crea : rival
+segunda sala crea :
+tercera sala crea el perdedor de primera sala
+13 Rondas
+200hp
+1500 monedas
+Municiones Limitadas (si)
+
+• Aclaraciones:
+
+Se espera solo 10 para el ID e ingresar
+Max 2 cambios de jugadores
+Max 1 emulador por equipo
+Solo una M1014 por equipo
+Desert solo la primera ronda
+No se vale ningun tipo de altura (por defecto Mill y Clock si)
+Clan todos en el mismo
+Team 2 con tag 
+cuentas nivel +45
+Sala mal creada se crea de nuevo 6 solo 5 min de tolerancia 
+El versus debe ser cancelado 1 hora antes con anticipación o se tomará como versus ganado 
+
+• Prohibido:
+Wuckong
+Cr7
+Dimitri 
+Skyler
+Cualquier tipo de explosivos
+Franco tiradores
+Chips
+Puño 
+Emotes
+doble vector
+
+
+• Habilidades:
+
+Únicas Habilidades permitidas son:
+ Alok o K como activas
+Moco, Kelly y Maxim como pasivas.
+`;
