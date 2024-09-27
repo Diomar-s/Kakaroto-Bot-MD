7d8c0583f22035734f345f138865c123448a9b34
Diomar-s <diomarlugo405@gmail.com>
2024-09-27 02:02:11 -0400

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -41,7 +41,7 @@ await conn.reply(m.chat, errorMessage2, m, rcanal,)
 
 }
 
-handler.help = ['update', 'actualizar']
+handler.help = ['update', 'up', 'actualizar']
 handler.tags = ['owner']
 handler.command = ['update', 'actualizar']
 handler.rowner = true
