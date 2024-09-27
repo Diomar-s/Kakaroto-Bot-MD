3ce391975450da14f15ee82e564133e270a4c640
Diomar-s <diomarlugo405@gmail.com>
2024-09-27 02:06:41 -0400

    Add files via upload

---
+++ plugins/ff-Reglas500.js
@@ -0,0 +1,53 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Infinito);
+};
+handler.command = /^(Infinito)$/i;
+handler.tags =['free'] 
+handler.help = ['Infinito'] 
+export default handler;
+global.Infinito = ` 
+*Sala*
+•1Ra sala crea: Rival
+•2da sala crea: 
+ (3ra sala crea perdedor de primera)
+
+*Configuración*
+•13 Rondas 
+•500 hp
+•1500 de moneda
+•Habilidad de personajes Si
+•Municiones *Ilimitadas*
+•Primera ronda Desert
+•Airdrops: No
+
+
+*Importante*
+•Vale encerrarse (pero no dejar fuera de zona)
+•Sola una m10 por equipo 
+•Si es clan, todos deben pertenecer al mismo.
+•2 cambios de jugadores por equipo
+•Solo 2 espectadores por equipo.
+•El versus debe ser cancelado 1 hora antes con anticipación o se tomará como versus ganado.
+Sala mal creada se crea de nuevo y solo 5 min de tolerancia.
+•Única altura válida: Cajas pequeñas, autos. 
+Clock, Mill y Observatorio no cuentan como altura 
+•Cuantas niveles +45
+•Como habilidades activas solo Alok y K
+•Como habilidades pasivas: Moco, Kelly, Maxim y Hayato 
+•Todo reclamo debe ser echo con fotos o videos claros y se dará sala ganada al afectado.
+•10 minutos de tolerancia una vez pasado el id de la sala
+
+
+
+
+*Prohibido*
+
+•Habilidades de: Cr7, Wikong, Dimitri y Skyler.
+•Doble vector (así sea una sola, tampoco se vale) Mag 7
+•Cualquier tipo de Granada ( Exepcto La Pared Gloo )
+•No puños 
+•No bug o hacker
+•No AR o Francotiradores
+•No matar cuando el oponente está bugueado
+•Chips
+`;
