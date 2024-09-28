096645a2ebae5ec74b899bf9088c71c27a489175
Diomar-s <diomarlugo405@gmail.com>
2024-09-28 00:45:14 -0400

    Update descargas-apkdl.js

--- plugins/descargas-apkdl.js
+++ plugins/descargas-apkdl.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} Clash Royale\`\`\``
-let res = await  
 let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
 let result = await res.json();
 let { name, size, lastUpdate, icon } = result;
