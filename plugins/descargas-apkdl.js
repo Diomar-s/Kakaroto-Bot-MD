a03d921871380f4a964131873cfb52182cf2aabc
Diomar-s <diomarlugo405@gmail.com>
2024-09-30 13:57:18 -0400

    Update descargas-apkdl.js

--- plugins/descargas-apkdl.js
+++ plugins/descargas-apkdl.js
@@ -15,6 +15,7 @@ await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
 await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: ''}, { quoted: m });
 }
 handler.command = ['apk', 'apkdl', 'modapk']
+handler.group = true;
 handler.help = ['apkdl']
 handler.tags = ['descargas']
 export default handler
