803ce89c0ab0b325e914a99a530bc157c0cb21cb
Diomar-s <diomarlugo405@gmail.com>
2024-10-14 15:49:42 -0400

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -56,29 +56,29 @@ setTimeout(resolve, 1000)
 let timestamp = speed()
 let latensi = speed() - timestamp
 let CrowBot = `╭─⬣「 *Info De Kakaroto* 」⬣\n`
-CrowBot += `│ 👑 *Creador* : @${owner[0][0].split('@s.whatsapp.net')[0]}\n`
-CrowBot += `│ 🍭 *Prefijo* : [  ${usedPrefix}  ]\n`
-CrowBot += `│ 🐉 *Total Plugins* : ${totalf}\n`
-CrowBot += `│ 💫 *Plataforma* : ${platform()}\n`
-CrowBot += `│ 🧿 *Servidor* : ${hostname()}\n`
-CrowBot += `│ 🚀 *RAM* : ${format(totalmem() - freemem())} / ${format(totalmem())}\n`
-CrowBot += `│ 🌟 *FreeRAM* : ${format(freemem())}\n`
-CrowBot += `│ ✨️ *Speed* : ${latensi.toFixed(4)} ms\n`
-CrowBot += `│ 🕗 *Uptime* : ${uptime}\n`
-CrowBot += `│ 💨 *Modo* : ${bot.public ? 'Privado' : 'Publico'}\n`
-CrowBot += `│ 💨 *Comandos Ejecutados* : ${toNum(totalStats)} ( *${totalStats}* )\n`
-CrowBot += `│ 🤖 *Grupos Registrados* : ${toNum(totalchats)} ( *${totalchats}* )\n`
-CrowBot += `│ 🍧 *Registrados* : ${toNum(totalreg)} ( *${totalreg}* ) Usuarios\n`
-CrowBot += `╰─⬣\n\n`
-CrowBot += `╭─⬣「 *Chats De Kakaroto* 」⬣\n`
-CrowBot += `│ 🧃 *${groupsIn.length}* Chats en Grupos\n`
-CrowBot += `│ 👾 *${groupsIn.length}* Grupos Unidos\n`
-CrowBot += `│ 👾 *${groupsIn.length - groupsIn.length}* Grupos Salidos\n`
-CrowBot += `│ 💬 *${chats.length - groupsIn.length}* Chats Privados\n`
-CrowBot += `│ 💭 *${chats.length}* Chats Totales\n`
-CrowBot += `╰─⬣\n\n`
-CrowBot += `╭─⬣「 *NodeJS Uso de memoria* 」⬣\n`
-CrowBot += `${'```' + Object.keys(used).map((key, _, arr) => `│ ${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}\n`
+GokuBot += `│ 👑 *Creador* : @+5491166401905\n`
+GokuBot += `│ 🍭 *Prefijo* : [  ${usedPrefix}  ]\n`
+GokuBot += `│ 🐉 *Total Plugins* : ${totalf}\n`
+GokuBot += `│ 💫 *Plataforma* : ${platform()}\n`
+GokuBot += `│ 🧿 *Servidor* : ${hostname()}\n`
+GokuBot += `│ 🚀 *RAM* : ${format(totalmem() - freemem())} / ${format(totalmem())}\n`
+GokuBot += `│ 🌟 *FreeRAM* : ${format(freemem())}\n`
+GokuBot += `│ ✨️ *Speed* : ${latensi.toFixed(4)} ms\n`
+GokuBot += `│ 🕗 *Uptime* : ${uptime}\n`
+GokuBot += `│ 💨 *Modo* : ${bot.public ? 'Privado' : 'Publico'}\n`
+GokuBot += `│ 💨 *Comandos Ejecutados* : ${toNum(totalStats)} ( *${totalStats}* )\n`
+GokuBot += `│ 🤖 *Grupos Registrados* : ${toNum(totalchats)} ( *${totalchats}* )\n`
+GokuBot += `│ 🍧 *Registrados* : ${toNum(totalreg)} ( *${totalreg}* ) Usuarios\n`
+GokuBot += `╰─⬣\n\n`
+GokuBot += `╭─⬣「 *Chats De Kakaroto* 」⬣\n`
+GokuBot += `│ 🧃 *${groupsIn.length}* Chats en Grupos\n`
+GokuBot += `│ 👾 *${groupsIn.length}* Grupos Unidos\n`
+GokuBot += `│ 👾 *${groupsIn.length - groupsIn.length}* Grupos Salidos\n`
+GokuBot += `│ 💬 *${chats.length - groupsIn.length}* Chats Privados\n`
+GokuBot += `│ 💭 *${chats.length}* Chats Totales\n`
+GokuBot += `╰─⬣\n\n`
+GokuBot += `╭─⬣「 *NodeJS Uso de memoria* 」⬣\n`
+GokuBot += `${'```' + Object.keys(used).map((key, _, arr) => `│ ${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}\n`
 CrowBot += `╰─⬣`
 
 await conn.reply(m.chat, CrowBot, fkontak, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎🚩˖ ⸙',body: packname, previewType: 0, thumbnail: icons, sourceUrl: redes}}})
