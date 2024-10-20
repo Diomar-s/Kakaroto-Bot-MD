f62f21ff6f27846e99d9087177e05b1789b27076
Diomar-s <diomarlugo405@gmail.com>
2024-10-19 23:04:27 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -733,16 +733,16 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary*\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t.', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary*\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t.', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
-mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.**\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
 premium: '「👹」 *Esta función solo es para usuarios Premium.', 
-group: '「💨」 *Esta funcion solo puede ser ejecutada en grupos.**\n\n*_El nuevo grupo del bot*\n\n*UNETE* 👉https://chat.whatsapp.com/DrQmZfEMSWiK2COjH3Xqq7', 
-private: '「🍭」 *Esta función solo puede ser usada en chat privado.**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-admin: '「👑」 *Este comando solo puede ser usado por admins.**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-botAdmin: '「🔱」 *Para usar esta función debo ser admin.**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+group: '「💨」 *Esta funcion solo puede ser ejecutada en grupos.**\n\n*El nuevo grupo del bot*\n\n*UNETE* 👉https://chat.whatsapp.com/DrQmZfEMSWiK2COjH3Xqq7', 
+private: '「🍭」 *Esta función solo puede ser usada en chat privado.**\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+admin: '「👑」 *Este comando solo puede ser usado por admins.**\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+botAdmin: '「🔱」 *Para usar esta función debo ser admin.**\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
 unreg: '「💨」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg legendary.18**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t',
-restrict: '「💫」 *Esta característica esta desactivada.**\n\n*_Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t'
+restrict: '「💫」 *Esta característica esta desactivada.**\n\n*Siguenos ♥️🪄https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
 const file = global.__filename(import.meta.url, true);
