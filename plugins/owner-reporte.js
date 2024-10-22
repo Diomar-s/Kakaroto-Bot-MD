22137f4c47fe330e66f09daa09104cdd70a3f33a
Diomar-s <diomarlugo405@gmail.com>
2024-10-22 00:59:43 -0400

    Add files via upload

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw '⚠ *_️Ingrese el error ue desea reportar._*'
+    if (!text) throw '⚠ *_️Ingrese el error que desea reportar._*'
     if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
     if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
     const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
@@ -8,6 +8,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['reportar']
 handler.tags = ['info']
-handler.command = ['reporte','report','reportar','bug','error']
+handler.command = /^(reporte|report|reportar|bug|error)$/i
 
 export default handler
---
+++ plugins/owner_add_del_owner.js
@@ -0,0 +1,25 @@
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  const why = `*[❗] Uso incorrecto del comando.*\n\n*—◉ Ejemplo de uso válido:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} _mensaje citado_*`;
+  const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
+  if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
+  switch (command) {
+    case 'addowner':
+      const nuevoNumero = who;
+      global.owner.push([nuevoNumero]);
+      await conn.reply(m.chat, '*🦁 Listo Amo, El Usuario Ya Está En La Lista De Owner.*', m);
+      break;
+    case 'delowner':
+      const numeroAEliminar = who;
+      const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
+      if (index !== -1) {
+        global.owner.splice(index, 1);
+        await conn.reply(m.chat, '*⚡️ El Numero fue eliminado de la lista de owner correctamente.*', m);
+      } else {
+        await conn.reply(m.chat, '*⏳️ El Numero No Está En La Lista De Owners.*', m);
+      }
+      break;
+  }
+};
+handler.command = /^(addowner|delowner)$/i;
+handler.rowner = true;
+export default handler;
