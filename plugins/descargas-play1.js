6f252afc77b6eeba9a06f6a3dff156d9f5569a65
Diomar-s <diomarlugo405@gmail.com>
2024-10-22 16:40:43 -0400

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -7,7 +7,7 @@ const randomReduction = Math.floor(Math.random() * 5) + 1;
     let f = `\n\n${String.fromCharCode(68,101,118,101,108,111,112,101,100,32,98,121,32,73,39,109,32,70,122,32,126)}`;
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
-    let body = `\`\`\`⊜─⌈ 📻 ◜YouTube Play◞ 📻 ⌋─⊜
+
     
 let texto1 = `╭────୨ৎ────────୨ৎ────✩°｡ ⋆⸜ 🎧✮
  │ *∘ 🍭 descarga* 
