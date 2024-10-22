8806842ff1f427ed5fdb1aedf9d3aa99a94b9130
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-22 01:54:38 -0400

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -17,7 +17,7 @@ conn.reply(m.chat, `🎁 *Recompensa Diaria*
 Recursos:
 ✨ Xp : *+${isPrems ? exppremium : exp}*
 💎 Diamantes : *+${d}*
-❤️‍🔥 DragoCoins : *+${coin}*`, m, )
+🐉 DragoCoins : *+${coin}*`, m, )
 
 global.db.data.users[m.sender].lastclaim = new Date * 1
 
