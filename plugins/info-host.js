0f5188474e7a2396c4ec4597c6186278847a8474
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-22 20:49:29 -0400

    Add files via upload

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,39 +1,70 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `🚩 *CORINPLUS - H O S T* 
+let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-*¿Quieres un Host de calidad y con bajos precios?*
-Pues te presento a *Corinplus*, un hosting de calidad con servidores dedicados y precios por debajo de 1USD, estos servidores están destinados a ofrecerte un Uptime 24/7 para que puedas alojar tus proyectos y qué estos funcionen de manera eficaz.
+let txt = `☁️ *S K Y - U L T R A - P L U S* ☁️
 
-🟢 \`\`\`Información del Host\`\`\`
+¿Tu Nokia es muy lento y necesitas que tu bot esté activo 24/7? 📱⏳
 
-🔮 *Dashboard:* 
-• https://dash.corinplus.com
+¡Tenemos la solución perfecta para ti! 🎉 Mantén tu bot funcionando sin interrupciones con nuestros servidores, Ofrecemos servidores gratuitos y de pago a precios súper accesibles, al alcance de todos. 💸 
 
-🧃 *Panel:*
-• https://ctrl.corinplus.com
+🖥️ *Totalmente compatible con Kakaroto-Bot-MD:* Disfruta al máximo de su potencial en nuestros servidores de alto rendimiento, asegurando una experiencia fluida y de alta calidad. El staff de Kakaroto-Bot-MD y Sky Ultra Plus Host se encarga de que disfrutes de todas sus funciones al máximo. ✨
 
-💥 *Canal:*
-• https://whatsapp.com/channel/0029VakUvreFHWpyWUr4Jr0g
+🔮 \`\`\`Información del Host\`\`\`
 
-⚜️ *Contacto (Marito)*
-https://wa.me/5492266613038
+💻 *Página:*
 
-> *Únete a está comunidad y disfruta de un servicio de calidad :D*` 
+https://dash.skyultraplus.com
+
+*🟢 Dashboard:*
+
+https://dash.skyultraplus.com
+
+⚙️ *Panel*
+
+https://panel.skyultraplus.com
+
+🌟 *Comunidad de WhatsApp:*
+
+https://chat.whatsapp.com/KGPhTIfgOzZCMNqoc3R7OW
+
+*🔵 Discord:*
+
+https://discord.gg/zvKgtc2RBc
+
+💚 *Canal de WhatsApp:*
+
+https://whatsapp.com/channel/0029VakUvreFHWpyWUr4Jr0g
+
+🗣📲 *Contacto:*
+
+• wa.me/5214531287294
+
+• wa.me/15167096032
+
+• wa.me/573147616444
+
+No esperes más y lleva tu bot al siguiente nivel con nuestro servicio de alojamiento. ¡Es fácil, rápido y económico! ✨🚀` 
+
 await conn.sendMessage(m.chat, { text: txt,
 contextInfo:{
+forwardedNewsletterMessageInfo: { 
+newsletterJid: '120363301598733462@newsletter', 
+serverMessageId: '', 
+newsletterName: 'Sky-Ultra-Plus ☁️' }, 
 forwardingScore: 9999999,
-isForwarded: false, 
+isForwarded: true, 
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🔵 C O R I N P L U S - H O S T 🔵`,
-body: `⚜️ Super Hosting 24/7 ⚜️`,
+title: `☁️ 𝐒𝐊𝐘𝐏𝐋𝐔𝐒-𝐇𝐎𝐒𝐓 ☁️`,
+body: `¡El plus que necesitas!`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://qu.ax/waCu.jpg', 
-sourceUrl: 'https://dash.corinplus.com'}}},
+thumbnailUrl: 'https://qu.ax/wXciz.jpg', 
+sourceUrl: redeshost}}},
 { quoted: fkontak})
 }
-handler.tags =['main'] 
-handler.help = ['servidor', 'corinplus'] 
-handler.command = ['host', 'corin', 'corinplus', 'servidor']
+
+handler.help = ['skyplus']
+handler.tags = ['info']
+handler.command = ['skyplus', 'skyultra', 'skyultraplus', 'sky', 'hosting', 'host']
+
 export default handler
