cd3716aa2364bff84b07b0e9e2d0cc2981a19b9c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-29 08:42:55 -0400

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -1,15 +1,11 @@
-//import db from '../lib/database.js'
-
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
+const handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sBye = text
-    m.reply('✅ Se estableció el mensaje de despedida')
-  } else throw `✳️ introduce el mensaje\n@user (mención)`
+    global.db.data.chats[m.chat].bye = text;
+    m.reply('✅ Se estableció el mensaje de despedida');
+  } else throw `✳️ introduce el mensaje\n@user (mención)`;
 }
-handler.help = ['setbye <text>']
-handler.tags = ['grupo']
-handler.command = ['setbye'] 
-handler.admin = true
-handler.owner = false
-
-export default handler
+handler.help = ['setbye <text>'];
+handler.tags = ['grupo'];
+handler.command = ['setbye'];
+handler.admin = true;
+export default handler;
