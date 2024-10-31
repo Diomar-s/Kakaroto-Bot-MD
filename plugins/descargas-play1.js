7dfd217e56d7fd456eb1d923bbd16eb4d875bd46
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-31 02:36:16 -0400

    Add files via upload

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -1,12 +1,12 @@
-import yts from 'yt-search' 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `\`\`\`[ 🌴 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
+import yts from 'yt-search'; 
 
-    const randomReduction = Math.floor(Math.random() * 5) + 1;
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `\`\`\`[ 🐉 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
+
     let search = await yts(text);
-    let f = `\n\n${String.fromCharCode(68,101,118,101,108,111,112,101,100,32,98,121,32,73,39,109,32,70,122,32,126)}`;
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
+    
     let body = `\`\`\`⊜─⌈ 📻 ◜YouTube Play◞ 📻 ⌋─⊜
 
     ≡ Título : » ${search.all[0].title}
@@ -15,28 +15,27 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     ≡ Uploaded : » ${search.all[0].ago}
     ≡ URL : » ${urls}
 
-# 🌴 Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...\`\`\``;
+# 🐉 Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...\`\`\``;
     
-    conn.sendMessage(m.chat, { 
+    await conn.sendMessage(m.chat, { 
         image: { url: search.all[0].thumbnail }, 
-        caption: body + f
+        caption: body + `\n\n> 𝓚𝓪𝓴𝓪𝓻𝓸𝓽𝓸-𝓑𝓸𝓽-𝓜𝓓`
     }, { quoted: m });
 
-    let res = await dl_vid(urls)
+    let res = await dl_vid(urls);
     let type = isVideo ? 'video' : 'audio';
-    let video = res.data.mp4;
-    let audio = res.data.mp3;
-    conn.sendMessage(m.chat, { 
-        [type]: { url: isVideo ? video : audio }, 
+    let mediaUrl = isVideo ? res.data.mp4 : res.data.mp3;
+
+    await conn.sendMessage(m.chat, { 
+        [type]: { url: mediaUrl }, 
         gifPlayback: false, 
         mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
     }, { quoted: m });
 }
 
-handler.command = ['play', 'playvid'];
-handler.help = ['play', 'playvid'];
+handler.command = ['play1', 'playvid'];
+handler.help = ['play1', 'playvid'];
 handler.tags = ['descargas'];
-handler.group = true
 export default handler;
 
 async function dl_vid(url) {
@@ -47,15 +46,12 @@ async function dl_vid(url) {
             'api_key': 'free',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            text: url,
-        })
+        body: JSON.stringify({ text: url })
     });
 
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const data = await response.json();
-    return data;
+    return await response.json();
 }
