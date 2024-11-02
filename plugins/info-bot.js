01c0c2758c43cd9b29e02a9b4b98c94b2f0cb8f4
Diomar-s <diomarlugo405@gmail.com>
2024-11-02 16:37:34 -0400

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,10 +5,10 @@ handler.all = async function(m) {
 
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
-if (/^bot$/i.test(m.text)) {
+if (/^bro$/i.test(m.text)) {
 conn.reply(m.chat, `👻 ¡Hola! Soy Kakatoto-Bot, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
- if (/^grupo|aceptar|tenerunbot|como puedo tener el bot en mi grupo/i.test(m.text)) {
+ if (/^grupo|aceptar|tenerunbot|como puedo tener el bot en mi grupo|bot/i.test(m.text)) {
 conn.reply(m.chat, `**¿QUIERES A KAKAROTO-BOT EN TU GRUPOS?*🔥 
 
 !! *Pues es fácil lo único que pedimos*!!🙂‍↔️
