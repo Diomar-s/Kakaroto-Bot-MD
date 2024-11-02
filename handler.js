cb956b75903f512bbf030df89fc209346c0b8432
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-02 18:06:46 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -745,13 +745,13 @@ global.dfail = (type, m, conn) => {
 const msg = {
 rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t.', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
-mods: '「🤴🏻」 *Esta función solo puede ser usada por mis desarrolladores.*\n\n* *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-premium: '「👹」 *Esta función solo es para usuarios Premium.', 
+mods: '「👨‍💻」 *Esta función solo puede ser usada por mis desarrolladores.*\n\n* *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+premium: '「⚜️」 *Esta función solo es para usuarios Premium.', 
 group: '「💨」 *Este comando solo puede ser utilizada en grupos.*\n\n*UTILIZA EL COMANDO* _.grupos_ \n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-private: '「🍭」 *Esta función solo puede ser usada en chat privado.*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+private: '「🐉」 *Esta función solo puede ser usada en chat privado.*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
 admin: '「👑」 *Este comando solo puede ser usado por admins.*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
 botAdmin: '「🔱」 *Para usar esta función debo ser admin.*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-unreg: '「💨」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*#reg nombre.edad*\n\n*_❕ Ejemplo_* \n\n*#reg legendary.18*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t',
+unreg: '「☁️」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*#reg nombre.edad*\n\n*_❕ Ejemplo_* \n\n*#reg legendary.18*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t',
 restrict: '「💫」 *Esta característica esta desactivada.*\n\n *Nos ayuda siguiendo*🙏\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
