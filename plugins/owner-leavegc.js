a85b83e1353d0bbb1ab7fc9cbfa6178b1e902cf8
Diomar-s <diomarlugo405@gmail.com>
2024-11-02 23:45:23 -0400

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `🚩 *Megumin-Bot* Abandona El Grupo, Fué Genial Estar Aquí 👋`) 
+await conn.reply(id, `🚩 *Kakaroto-Bot* Abandona El Grupo, Fué Genial Estar Aquí 👋`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
