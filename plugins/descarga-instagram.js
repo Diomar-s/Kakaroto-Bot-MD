0a97d973cf4a19e4993125cfc912ecff8b4bcc27
Diomar-s <diomarlugo405@gmail.com>
2024-11-03 22:23:02 -0400

    Update and rename instagram-descarga.js to descarga-instagram.js

--- plugins/descarga-instagram.js
+++ plugins/descarga-instagram.js
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🌸 *Tu video de instagram.*\n' + textbot, fkontak)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '💥 *Tu video de instagram.*\n' + textbot, fkontak)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '❌ Ocurrió un error.', m, fake)}}
