3e4ec91cb93248e215ac887305e5222508de3624
Diomar-s <diomarlugo405@gmail.com>
2024-11-03 23:18:31 -0400

    Update ia-dalle.js

--- plugins/ia-dalle.js
+++ plugins/ia-dalle.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
         throw `Error...`;
     }
 }
-handler.tags = ['ia']
-handler.help = ['ia']
+handler.tags = ['dalle']
+handler.help = ['ai']
 handler.command = ['dalle'];
 export default handler;
