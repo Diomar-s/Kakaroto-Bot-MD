469413d652c998755f3d7cddc19780a7be428214
Diomar-s <diomarlugo405@gmail.com>
2024-11-05 05:32:22 -0400

    Update jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -15,7 +15,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
 
   let replyMessage = message.length === 0 ? '' : message
   global.totalUsers = users.length
-  let responseMessage = `╭━〔 𝗦𝗨𝗕-𝗕𝗢𝗧𝗦 𝗝𝗔𝗗𝗜𝗕𝗢𝗧 🤍 〕⬣\n┃ *𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝚂𝚄𝙱𝙱𝙾𝚃𝚂* : ${totalUsers || '0'}\n╰━━━━━━━━━━━━⬣\n\n${replyMessage.trim()}`.trim()
+  let responseMessage = `╭━〔 🐉 𝙎𝙐𝘽𝘽𝙊𝙏𝙎 - 𝙆𝘼𝙆𝘼𝙍𝙊𝙏𝙊 🐉 〕⬣\n┃ *ՏᑌᗷᗷOTՏ ᑕOᑎᗴᑕTᗩᗪOՏ* : ${totalUsers || '0'}\n╰━━━━━━━━━━━━⬣\n\n${replyMessage.trim()}`.trim()
 
 await stars.sendMessage(m.chat, { text: responseMessage, mentions: stars.parseMention(responseMessage) }, { quoted: fkontak })
 // await conn.reply(m.chat, responseMessage, m, rcanal)
