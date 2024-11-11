8d0e39bcedc686f67aedbc10a038ec4f22597865
Diomar-s <diomarlugo405@gmail.com>
2024-11-10 22:55:49 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -743,16 +743,16 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary*\n\n *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t.', 
+rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> legendary.', 
 owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
-mods: '「👨‍💻」 *Esta función solo puede ser usada por mis desarrolladores.*\n\n* *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
+mods: '「👨‍💻」 *Esta función solo puede ser usada por mis desarrolladores*.', 
 premium: '「⚜️」 *Esta función solo es para usuarios Premium.', 
 group: '「💨」 *Este comando solo puede ser utilizada en grupos.*\n\n*UTILIZA EL COMANDO* _.grupos_ \n\n *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-private: '「🐉」 *Esta función solo puede ser usada en chat privado.*\n\n *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-admin: '「👑」 *Este comando solo puede ser usado por admins.*\n\n *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-botAdmin: '「🔱」 *Para usar esta función debo ser admin.*\n\n *CANAL DE ANIME 💫 *\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t', 
-unreg: '「☁️」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*#reg nombre.edad*\n\n*_❕ Ejemplo_* \n\n*#reg legendary.18*\n\n *CANAL ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t',
-restrict: '「💫」 *Esta característica esta desactivada.*\n\n *CANAL DE ANIME 💫*\n\n https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t'
+private: '「🐉」 *Esta función solo puede ser usada en chat privado.', 
+admin: '「👑」 *Este comando solo puede ser usado por admins.', 
+botAdmin: '「🔱」 *Para usar esta función debo ser admin.', 
+unreg: '「☁️」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*#reg nombre.edad*\n\n*_❕ Ejemplo_* \n\n*#reg legendary.18',
+restrict: '「💫」 *Esta característica esta desactivada*.'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
 const file = global.__filename(import.meta.url, true);
