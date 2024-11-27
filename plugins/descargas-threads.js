7da89d7e068953f06c79b18b12b3c81306738800
Niño Piña <ninopina10@gmail.com>
2024-11-26 20:55:20 -0600

    Actualizar descargas-threads.js

--- plugins/descargas-threads.js
+++ plugins/descargas-threads.js
@@ -31,7 +31,7 @@ let handler = async (m, { args, conn }) => {
 }
 
 handler.help = ['threads *<link>*'];
-handler.corazones = 2;
+handler.dragones = 3;
 handler.tags = ['descargas'];
 handler.command = ['threads', 'thdl', 'thread'];
 
