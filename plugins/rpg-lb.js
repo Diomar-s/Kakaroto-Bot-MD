f2f7ad69a858aed6c8a86a2b3cc164fa81475699
Diomar-s <diomarlugo405@gmail.com>
2024-12-06 22:39:07 -0400

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -10,10 +10,10 @@ let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 
 let text = `
-╭───═[ *Top ${len} Yenes 💴* ]═────⋆
+╭───═[ *Top ${len} dragones 🐉* ]═────⋆
 │╭───────────────···
 ││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-││ ${sortedLim.slice(0, len).map(({ jid, yenes }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${yenes} 💴*`).join`\n││ `}
+││ ${sortedLim.slice(0, len).map(({ jid, dragones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${dragones} 🐉*`).join`\n││ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
