134b0eac37a607b3e4be175bb225da9ed657c82a
Diomar-s <diomarlugo405@gmail.com>
2024-12-06 23:36:06 -0400

    Update economy-casino.js

--- plugins/economy-casino.js
+++ plugins/economy-casino.js
@@ -21,18 +21,18 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + `💴 *Yenes*` + ' que deseas aportar contra' + ` *Kakaroto-Bot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
-if (user.yenes >= count * 1) {
-user.yenes -= count * 1
+if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + `🐉 *dragones*` + ' que deseas aportar contra' + ` *Kakaroto-Bot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (user.dragones >= count * 1) {
+user.dragones -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`🌟 Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 💴 Yenes.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🌟 Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🐉 dragones.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
-user.yenes += count * 2
-conn.reply(m.chat, '`✨ Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 💴 Yenes.`.trim(), m, rcanal)
+user.dragones += count * 2
+conn.reply(m.chat, '`✨ Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🐉 dragones.`.trim(), m, rcanal)
 } else {
-user.yenes += count * 1
-conn.reply(m.chat, '`🌟 Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 💴 Yenes.`.trim(), m, rcanal)}
-} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 💴 Yenes* para apostar!`.trim(), m, rcanal)}
+user.dragones += count * 1
+conn.reply(m.chat, '`🌟 Veamos que numeros tienen!`\n\n'+ `➠ *Kakaroto* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🐉 dragones.`.trim(), m, rcanal)}
+} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🐉 dragones* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
 handler.tags = ['economy']
