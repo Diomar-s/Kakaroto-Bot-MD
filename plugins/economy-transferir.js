a4ff236919fb8cb8ec589c741a3baf859050b39d
Diomar-s <diomarlugo405@gmail.com>
2024-12-29 20:07:45 -0400

    Update economy-transferir.js

--- plugins/economy-transferir.js
+++ plugins/economy-transferir.js
@@ -1,4 +1,4 @@
-const items = ['yenes', 'exp'];
+const items = ['dragones', 'exp'];
 const confirmation = {};
 
 async function handler(m, { conn, args, usedPrefix, command }) {
@@ -11,7 +11,7 @@ async function handler(m, { conn, args, usedPrefix, command }) {
 *┃☁️ 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:* ${usedPrefix + command} exp 65 @${m.sender.split('@')[0]}
 ┣❣◤▬▭▬▭▬ ◆ ▬▭▬▭▬ ◤❢
 *┃◉ ⚜️ 𝑨𝒓𝒕𝒊𝒄𝒖𝒍𝒐𝒔 𝒕𝒓𝒂𝒏𝒔𝒇𝒆𝒓𝒊𝒃𝒍𝒆𝒔.*
-┃▢ *yenes* = 𝑌𝑒𝑛𝑒𝑠 💴
+┃▢ *dragones* = dragones 🐉
 ┃▢ *exp* = 𝑬𝒙𝒑𝒆𝒓𝒊𝒆𝒏𝒄𝒊𝒂
 *┗━┅┉┅┄┈┄⟞⟨⟠⟩⟝┄┈┄┉┉┉━┛*
 `.trim();
